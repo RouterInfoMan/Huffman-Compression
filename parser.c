5aeccdc0f5ccd5dc3bef8ef4463cfbee7fef3c54
Razvan-Andrei Rotaru <razvan.rotaru1601@stud.acs.upb.ro>
2022-12-16 21:55:36 +0200

    Made a bitwise parser

--- parser.c
+++ parser.c
@@ -8,7 +8,6 @@ void input_parser_init(input_parser *par, FILE *file) {
     par->p = BUF_SIZE - 1;
     par->eofp = BUF_SIZE + 1;
 }
-
 int read_uchar(input_parser *par, unsigned char *c) {
     par->p++;
     if (par->p == BUF_SIZE) {
@@ -54,35 +53,36 @@ void input_parser_delete(input_parser *par) {
     free(par);
     par = NULL;
 }
-void output_parser_init(output_parser *par, FILE *file) {
-    if (!file) {
-        printf("Invalid file!\n");
+
+void bit_parser_init(bit_parser *par, FILE *in, FILE *out, input_parser *par2) {
+    par->ipos = 7;
+    par->opos = 0;
+    par->par = par2;
+    
+    par->in = in;
+    par->out = out;
+}
+int get_bit(bit_parser *par) {
+    par->ipos++;
+    if (par->ipos == 8) {
+        par->ipos = 0;
+        read_uchar(par->par, &par->ibyte);
     }
-    par->buf = 0;
-    par->mp = 7;
+    return (par->ibyte & (1 << par->ipos)) >> par->ipos;
 }
-void output_parser_delete(output_parser *par) {
-    fclose(par->file);
-    free(par);
-    par = NULL;
-}
-void put_bit(output_parser *par, int bit) {
-    par->mp++;
-    if (par->mp == 8) {
-        par->mp = 0;
-        fputc(par->buf, par->file);
-        par->buf = 0;
+void put_bit(bit_parser *par, int bit) {
+    if (par->opos == 8) {
+        par->opos = 0;
+        printf("%c", par->obyte);
     }
     if (bit) {
-        par->buf |= 1 << par->mp;
+        par->obyte |= (1 << par->opos);
     } else {
-        par->buf &= ~(1 << par->mp);
+        par->obyte &= ~(1 << par->opos);
     }
+    par->opos++;
 }
-void flush(output_parser *par) {
-    if(par->mp) {
-        fputc(par->buf, par->file);
-        par->mp = 0;
-        par->buf = 0;
-    }
+void bit_flush(bit_parser *par) {
+    printf("%c", par->obyte);
+    par->opos = 0;
 }
